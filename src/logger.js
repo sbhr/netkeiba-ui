01274da973004482781be9a9a1b2c22b2dde8144
sbhr <test@test.com>
2017-02-27 00:26:01 +0900

    ロギング用モジュール

---
+++ src/logger.js
@@ -0,0 +1,23 @@
+const log4js = require('log4js');
+
+const logger = exports;
+
+log4js.configure({
+  appenders: [
+    {
+      type: 'dateFile',
+      category: 'scrape',
+      filename: './logs/scrape.log',
+      pattern: '-yyyy-MM-dd',
+    },
+    {
+      type: 'dateFile',
+      category: 'error',
+      filename: './logs/error.log',
+      pattern: '-yyyy-MM-dd',
+    },
+  ],
+});
+
+logger.scrape = log4js.getLogger('scrape');
+logger.error = log4js.getLogger('error');
