44f927ef617ad1aabff5b5396b87aa61bff42617
sbhr <test@test.com>
2017-03-12 12:38:51 +0900

    cheerio-httpcliのfetchをfetchSyncに変更 非同期だとログがうまく出力できなそう

--- src/scrape.js
+++ src/scrape.js
@@ -62,65 +62,65 @@ const getShutubaUrl = (raceUrl) => {
 };
 
 const scrapeShutubaTable = (shutubaUrl, idxOfDay) => {
-  client.fetch(shutubaUrl, (err, $) => {
-    logger.scrape.info(`fetch scrapeShutubaTable ${shutubaUrl}`);
-    const racePlace = $('.race_place .active').text();
-    raceData[racePlace] = Object.prototype.hasOwnProperty.call(raceData, racePlace) ? raceData[racePlace] : [];
-    const race = {};
-    race.num = delNL(delNL($('.racedata dt').text())).replace('R', '');
-    race.name = $('.racedata dd h1').text();
-    const status = $('.racedata dd p span').text();
-    race.type = status.slice(0, status.indexOf('m')).slice(0, 1);
-    race.distance = status.slice(0, status.indexOf('m')).slice(1);
-    race.horses = [];
-    for (let i = 0; i < $('.shutuba_table tr').length - 1; i += 1) {
-      race.horses.push({});
+  const shutubaData = client.fetchSync(shutubaUrl);
+
+  logger.scrape.info(`fetch scrapeShutubaTable ${shutubaUrl}`);
+  const racePlace = shutubaData.$('.race_place .active').text();
+  raceData[racePlace] = Object.prototype.hasOwnProperty.call(raceData, racePlace) ? raceData[racePlace] : [];
+  const race = {};
+  race.num = delNL(delNL(shutubaData.$('.racedata dt').text())).replace('R', '');
+  race.name = shutubaData.$('.racedata dd h1').text();
+  const status = shutubaData.$('.racedata dd p span').text();
+  race.type = status.slice(0, status.indexOf('m')).slice(0, 1);
+  race.distance = status.slice(0, status.indexOf('m')).slice(1);
+  race.horses = [];
+  for (let i = 0; i < shutubaData.$('.shutuba_table tr').length - 1; i += 1) {
+    race.horses.push({});
+  }
+  shutubaData.$('.shutuba_table tr').each((idx, elem) => {
+    if (idx > 0) {
+      race.horses[idx - 1].waku = elem.firstChild.next.attribs.class.replace('waku', '');
     }
-    $('.shutuba_table tr').each((idx, elem) => {
-      if (idx > 0) {
-        race.horses[idx - 1].waku = elem.firstChild.next.attribs.class.replace('waku', '');
+  });
+  shutubaData.$('.shutuba_table tr .umaban').each((idx, elem) => {
+    race.horses[idx].umaban = elem.firstChild.data;
+  });
+  shutubaData.$('.shutuba_table tr .txt_smaller').each((idx, elem) => {
+    race.horses[idx].blood = {
+      father: delNL(elem.firstChild.data),
+      mother: delNL(elem.firstChild.next.next.data),
+      detail: [],
+    };
+  });
+  shutubaData.$('.shutuba_table tr .h_name a').each((idx, elem) => {
+    race.horses[idx].name = elem.firstChild.data;
+    const bloodUrl = makeBloodUrl(elem.attribs.href);
+    const ret = client.fetchSync(bloodUrl);
+    logger.scrape.info(`fetchSync blood ${bloodUrl}`);
+    ret.$('.blood_table td a').each((i, element) => {
+      try {
+        const horseName = element.children[0].data;
+        if (horseName !== '産駒' && horseName !== '血統' && horseName !== undefined) {
+          race.horses[idx].blood.detail.push(delNL(delNL(horseName)));
+        }
+      } catch (e) {
+        logger.error.error(e);
       }
     });
-    $('.shutuba_table tr .umaban').each((idx, elem) => {
-      race.horses[idx].umaban = elem.firstChild.data;
-    });
-    $('.shutuba_table tr .txt_smaller').each((idx, elem) => {
-      race.horses[idx].blood = {
-        father: delNL(elem.firstChild.data),
-        mother: delNL(elem.firstChild.next.next.data),
-        detail: [],
-      };
-    });
-    $('.shutuba_table tr .h_name a').each((idx, elem) => {
-      race.horses[idx].name = elem.firstChild.data;
-      const bloodUrl = makeBloodUrl(elem.attribs.href);
-      const ret = client.fetchSync(bloodUrl);
-      logger.scrape.info(`fetchSync blood ${bloodUrl}`);
-      ret.$('.blood_table td a').each((i, element) => {
-        try {
-          const horseName = element.children[0].data;
-          if (horseName !== '産駒' && horseName !== '血統' && horseName !== undefined) {
-            race.horses[idx].blood.detail.push(delNL(delNL(horseName)));
-          }
-        } catch (e) {
-          logger.error.error(e);
-        }
-      });
-    });
-    const raceDatesStr = getRaceDate(new Date());
-    try {
-      db.insert({
-        date: raceDatesStr[idxOfDay],
-        place: racePlace,
-        num: race.num,
-        data: race,
-      });
-    } catch (e) {
-      logger.error.error(e);
-    }
-    raceData[racePlace].push(race);
-    fs.writeFile(outputFile, JSON.stringify(raceData, null, 2));
   });
+  const raceDatesStr = getRaceDate(new Date());
+  try {
+    db.insert({
+      date: raceDatesStr[idxOfDay],
+      place: racePlace,
+      num: race.num,
+      data: race,
+    });
+  } catch (e) {
+    logger.error.error(e);
+  }
+  raceData[racePlace].push(race);
+  fs.writeFile(outputFile, JSON.stringify(raceData, null, 2));
 };
 
 const raceDates = getRaceDateForUrl(new Date());
