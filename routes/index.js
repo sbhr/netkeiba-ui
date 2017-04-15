0f5a68592caab9c9c96ab8b65387000153281c40
sbhr <test@test.com>
2017-04-16 02:12:14 +0900

    fix for eslint & add controllers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const router = express.Router();
+const index = require('../controllers/index');
+
+router.get('/', index.getIndex);
 
 module.exports = router;
